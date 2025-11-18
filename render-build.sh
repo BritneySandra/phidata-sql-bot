3eeef0a626feb2764d60301f8482727c198ddeb0
Britney Sandra <bsandy2015@gmail.com>
2025-11-18 15:12:38 +0530

    Force Python 3.12 & fix pyodbc build

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y unixodbc unixodbc-dev
+apt-get install -y unixodbc unixodbc-dev g++ build-essential
+pip install --upgrade pip
 pip install -r requirements.txt
