d6f11c6b9d0ea444c96125c54e177720c77693c1
Britney Sandra <bsandy2015@gmail.com>
2025-11-18 16:47:33 +0530

    Fix ODBC driver install for Railway

---
+++ install-odbc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y curl apt-transport-https gnupg
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/12/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-python3 --version
-apt-get update
-apt-get install -y unixodbc unixodbc-dev g++ build-essential
-pip install --upgrade pip
-pip install -r requirements.txt
